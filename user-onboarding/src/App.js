51e39e3f519f9f9ad0d8439eeec38c04ddf06ced
Victor Tran <victor.n.tran@outlook.com>
2019-11-07 19:35:26 -0800

    generate users based on data submission

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import EnchancedForm from "./components/Form";
 
 function App() {
+  const [users, setUser] = useState([]);
+
+  const addUser = (user) => {
+    const newUsers = [...users, user];
+    setUser(newUsers);
+  }
+
+  const generateUserCards = () => {
+    const cards = users.length > 0 ? users.map(({id, name, email, createdAt}) => {
+      return (
+        <section className='card'>
+          <div className='id'>{id}</div>
+          <div className='name'>{name}</div>
+          <div className='email'>{email}</div>
+          <div className='createdAt'>{createdAt}</div>
+        </section>
+      )
+    }) : null;
+    return cards;
+  }
+
+
   return (
     <div className="App">
-      <EnchancedForm />
+      <EnchancedForm addUser={addUser}/>
+      <br />
+      {users && generateUserCards()}
     </div>
   );
 }
